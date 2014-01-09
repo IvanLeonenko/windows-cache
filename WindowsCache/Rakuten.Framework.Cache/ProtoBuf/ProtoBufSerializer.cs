004c82adc7264467299225e90496afe9a1c04ad9
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-09 10:33:50 +0900

    Save type properties mappings (protobuf indices)

--- WindowsCache/Rakuten.Framework.Cache/ProtoBuf/ProtoBufSerializer.cs
+++ WindowsCache/Rakuten.Framework.Cache/ProtoBuf/ProtoBufSerializer.cs
@@ -1,4 +1,5 @@
-﻿using ProtoBuf;
+﻿using System.Linq;
+using ProtoBuf;
 using ProtoBuf.Meta;
 using System;
 using System.Collections.Generic;
@@ -14,12 +15,16 @@ namespace Rakuten.Framework.Cache.ProtoBuf
         private static readonly Dictionary<Type, int> TypeToPropertyIndex = new Dictionary<Type, int>();
         private static readonly Dictionary<Type, int> TypeToSubtypeIndex = new Dictionary<Type, int>();
         private readonly IStorage _storage;
-        private List<Type> _userTypes { get; set; }
+        private ProtoBufMappings _protoBufMappings = new ProtoBufMappings();
+        private List<Type> _userTypes;
 
         public ProtoBufSerializer(IStorage storage, List<Type> userTypes = null)
         {
             _storage = storage;
             _userTypes = userTypes;
+
+            RestoreMappings();
+            
             if (_userTypes == null) 
                 return;
             foreach (var userType in _userTypes)
@@ -56,17 +61,100 @@ namespace Rakuten.Framework.Cache.ProtoBuf
         
         public void RegisterType(Type type)
         {
-            if (!RuntimeTypeModel.Default.CanSerializeBasicType(type))
+            //if (!RuntimeTypeModel.Default.CanSerializeBasicType(type))
+            //{
+            //    var metaType = RuntimeTypeModel.Default.Add(type, true);
+            //    AddProperties(type, metaType);
+            //}
+
+            //RegisterGenericCacheEntry(type);
+
+            UpdateMapping(type);
+            //RegisterGenericCacheEntry(type);
+            //UpdateMapping(CacheEntryTemplate.MakeGenericType(type));
+        }
+        
+        private void UpdateMapping(Type type)
+        {
+            if (RuntimeTypeModel.Default.CanSerializeBasicType(type))
+                return;
+
+            var typeName = type.FullName;
+            var mapping = _protoBufMappings.TypePropertiesIndices.ContainsKey(typeName) ? _protoBufMappings.TypePropertiesIndices[typeName] : new Dictionary<string, int>();
+            
+            var typeProperties = type.GetTypeInfo().DeclaredProperties.ToLookup(x => x.Name);
+
+            foreach (var item in mapping.Select(x => x.Key).Where(x => !typeProperties.Contains(x)).ToList())
             {
-                var metaType = RuntimeTypeModel.Default.Add(type, true);
-                AddProperties(type, metaType);
+                mapping.Remove(item);
+            }
+            
+            var metaType = RuntimeTypeModel.Default.Add(type, true);
+            
+            var maxInd = 0;
+            var addProperties = new List<string>();
+            foreach (var property in typeProperties)
+            {
+                if (mapping.ContainsKey(property.Key))
+                {
+                    var currentIndex = mapping[property.Key];
+                    if (currentIndex > maxInd)
+                        maxInd = currentIndex;
+                    metaType.Add(currentIndex, property.Key);
+                    metaType[currentIndex].IsRequired = false;
+                }
+                else
+                {
+                    addProperties.Add(property.Key);
+                }
             }
 
-            RegisterGenericCacheEntry(type);
-
-            if (!RuntimeTypeModel.Default.CanSerializeBasicType(type))
+            foreach (var property in addProperties)
             {
-                //save to 
+                mapping.Add(property, ++maxInd);
+                metaType.Add(maxInd, property);
+                metaType[maxInd].IsRequired = false;
+            }
+
+            _protoBufMappings.TypePropertiesIndices[typeName] = mapping;
+
+            
+            //todo: TypeSubTypesIndices should be cleaned up on initialization (e g cctor)
+            maxInd = 0;
+            var cacheEntryTypeName = typeof (ICacheEntry).FullName;
+            if (_protoBufMappings.TypeSubTypesIndices.ContainsKey(cacheEntryTypeName))
+            {
+                maxInd = _protoBufMappings.TypeSubTypesIndices[cacheEntryTypeName].Select(subtype => subtype.Value).Concat(new[] {maxInd}).Max();
+                maxInd++;
+                if (!_protoBufMappings.TypeSubTypesIndices[cacheEntryTypeName].ContainsKey(typeName))
+                    _protoBufMappings.TypeSubTypesIndices[cacheEntryTypeName].Add(typeName, maxInd);
+            }
+            else
+            {
+                maxInd++;
+                _protoBufMappings.TypeSubTypesIndices[cacheEntryTypeName] = new Dictionary<string, int> { { typeName, maxInd } };
+            }
+            RuntimeTypeModel.Default[typeof(ICacheEntry)].AddSubType(_protoBufMappings.TypeSubTypesIndices[cacheEntryTypeName][typeName], CacheEntryTemplate.MakeGenericType(type));
+            
+            SaveMappings();
+        }
+
+        private void RestoreMappings()
+        {
+            using (var stream = _storage.ReadStream("type.mappings.protobuf"))
+            {
+                if (stream != null && stream.Length > 0)
+                    _protoBufMappings = Deserialize<ProtoBufMappings>(stream);
+            }
+        }
+
+        private void SaveMappings()
+        {
+            if (_protoBufMappings != null && _protoBufMappings.TypePropertiesIndices.Count <= 0 && _protoBufMappings.TypeSubTypesIndices.Count <= 0) 
+                return;
+            using (var stream = Serialize(_protoBufMappings))
+            {
+                _storage.WriteStream("type.mappings.protobuf", stream);
             }
         }
 
