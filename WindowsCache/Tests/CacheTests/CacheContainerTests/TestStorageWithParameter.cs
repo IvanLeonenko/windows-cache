4dff56a0317bcc0be751616a2dd7552b159a0803
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-09 16:33:42 +0900

    Cache container unit tests

---
+++ WindowsCache/Tests/CacheTests/CacheContainerTests/TestStorageWithParameter.cs
@@ -0,0 +1,12 @@
+﻿using CacheTests.VersionTests;
+
+namespace CacheTests.CacheContainerTests
+{
+    class TestStorageWithParameter : TestStorage
+    {
+        public TestStorageWithParameter(string value)
+        {
+            KeyToStrings.Add("default", value);
+        }
+    }
+}
