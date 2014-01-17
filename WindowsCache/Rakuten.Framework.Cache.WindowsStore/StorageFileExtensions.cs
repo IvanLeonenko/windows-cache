63c5cefad5e767a10b5eef4759026d726baeee28
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-17 18:03:13 +0900

    Windows 8.0 library added IStorage implemented for windows 8.0

---
+++ WindowsCache/Rakuten.Framework.Cache.WindowsStore/StorageFileExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Rakuten.Framework.Cache.WindowsStore
+{
+    public static class StorageFileExtensions
+    {
+        public static async Task<string> ReadString(this IStorageFile file)
+        {
+            using (var stream = await file.OpenStreamForReadAsync())
+            {
+                TextReader reader = new StreamReader(stream);
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        public static async Task Write(this IStorageFile file, string content)
+        {
+            using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (var outputStream = fileStream.GetOutputStreamAt(0))
+                {
+                    using (var dataWriter = new DataWriter(outputStream))
+                    {
+                        dataWriter.WriteString(content);
+                        await dataWriter.StoreAsync();
+                        dataWriter.DetachStream();
+                    }
+
+                    await outputStream.FlushAsync();
+                }
+            }
+        }
+
+        public static async Task<byte[]> ReadBytes(this IStorageFile file)
+        {
+            using (var stream = await file.OpenStreamForReadAsync())
+            {
+                var bytes = new byte[stream.Length];
+                await stream.ReadAsync(bytes, 0, (int)stream.Length);
+                return bytes;
+            }
+        }
+
+        public static async Task Write(this IStorageFile file, byte[] content)
+        {
+            using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (var outputStream = fileStream.GetOutputStreamAt(0))
+                {
+                    using (var dataWriter = new DataWriter(outputStream))
+                    {
+                        dataWriter.WriteBytes(content);
+                        await dataWriter.StoreAsync();
+                        dataWriter.DetachStream();
+                    }
+
+                    await outputStream.FlushAsync();
+                }
+            }
+        }
+
+        public static async Task<Stream> ReadStream(this IStorageFile file)
+        {
+            using (var stream = await file.OpenStreamForReadAsync())
+            {
+                var bytes = new byte[stream.Length];
+                await stream.ReadAsync(bytes, 0, (int)stream.Length);
+                var memoryStream = new MemoryStream();
+                await memoryStream.WriteAsync(bytes, 0, bytes.Length);
+                return memoryStream;
+            }
+        }
+
+        public static async Task Write(this IStorageFile file, Stream content)
+        {
+            using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (var outputStream = fileStream.GetOutputStreamAt(0))
+                {
+                    using (var dataWriter = new DataWriter(outputStream))
+                    {
+                        var bytes = new byte[content.Length];
+                        await content.ReadAsync(bytes, 0, (int)content.Length);
+
+                        dataWriter.WriteBytes(bytes);
+                        await dataWriter.StoreAsync();
+                        dataWriter.DetachStream();
+                    }
+
+                    await outputStream.FlushAsync();
+                }
+            }
+        }
+    }
+}
---
+++ WindowsCache/Rakuten.Framework.Cache.WindowsStore/StoreStorage.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Rakuten.Framework.Cache.Storage;
+
+namespace Rakuten.Framework.Cache.WindowsStore
+{
+    public class StoreStorage : IStorage
+    {
+        private readonly string _cacheName;
+        private StorageFolder _storageFolder;
+        public StoreStorage(string cacheName)
+        {
+            _cacheName = cacheName;
+        }
+
+        private async Task<StorageFolder> GetWorkingFolder()
+        {
+            return _storageFolder ?? (_storageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(string.Format(@"Rakuten\cache\{0}", _cacheName), CreationCollisionOption.OpenIfExists));
+        }
+
+        public async Task WriteAsync(string key, string value)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.ReplaceExisting);
+            await file.Write(value);
+        }
+
+        public async Task<string> ReadStringAsync(string key)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.OpenIfExists);
+            return await file.ReadString();
+        }
+
+        public async Task WriteAsync(string key, byte[] value)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.ReplaceExisting);
+            await file.Write(value);
+        }
+
+        public async Task<byte[]> ReadBytesAsync(string key)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.OpenIfExists);
+            return await file.ReadBytes();
+        }
+
+        public async Task WriteAsync(string key, Stream value)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.ReplaceExisting);
+            await file.Write(value);
+        }
+
+        public async Task<Stream> ReadStreamAsync(string key)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.CreateFileAsync(key, CreationCollisionOption.OpenIfExists);
+            return await file.ReadStream();
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            var cacheFolder = await GetWorkingFolder();
+            var file = await cacheFolder.GetFileAsync(key);
+            await file.DeleteAsync();
+        }
+
+
+        public Stream GetStream(string key)
+        {
+            var cacheFolder = GetWorkingFolder();
+
+            throw new NotImplementedException();
+        }
+
+        public string GetString(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetBytes(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string key, Stream value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string key, byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
