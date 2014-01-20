2ec48f4db6048f30232e209e67e19816e3eeb89c
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-20 11:39:45 +0900

    Implement the rest classes needed for Win 8.0 store Factory

---
+++ WindowsCache/Rakuten.Framework.Cache.WindowsStore/WindowsStoreApplicationVersionProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Windows.ApplicationModel;
+
+namespace Rakuten.Framework.Cache.WindowsStore
+{
+    public class WindowsStoreApplicationVersionProvider : IVersionProvider
+    {
+        public Version GetVersion()
+        {
+            var packageVersion = Package.Current.Id.Version;
+            return new Version(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+        }
+    }
+}
---
+++ WindowsCache/Rakuten.Framework.Cache.WindowsStore/WindowsStoreCacheFactory.cs
@@ -0,0 +1,30 @@
+﻿using Rakuten.Framework.Cache.ProtoBuf;
+using Rakuten.Framework.Cache.Storage;
+using System;
+using System.Collections.Generic;
+
+namespace Rakuten.Framework.Cache.WindowsStore
+{
+    public class WindowsStoreCacheFactory : CacheFactory
+    {
+        public override CacheConfiguration GetDefaultCacheConfiguration()
+        {
+            return new CacheConfiguration(1024 * 1024 * 30, 1000, 1024 * 1024 * 30, 1000, TimeSpan.FromDays(14));
+        }
+
+        public override CacheConfiguration GetDefaultInMemoryCacheConfiguration()
+        {
+            return new CacheConfiguration(1024 * 1024 * 30, 1000, 1024 * 1024 * 30, 1000, TimeSpan.FromDays(14));
+        }
+
+        public override CacheContainer GetDefaultCacheContainer(IEnumerable<Type> userTypes, string cacheName)
+        {
+            var cacheContainer = new CacheContainer();
+            cacheContainer.Register<ILogger, StoreDebugCacheLogger>();
+            cacheContainer.Register<IVersionProvider, WindowsStoreApplicationVersionProvider>();
+            cacheContainer.Register<IStorage, StoreStorage>().WithValue("cacheName", cacheName);
+            cacheContainer.Register<ISerializer, ProtoBufSerializer>().WithDependency("storage", typeof(IStorage).FullName).WithValue("userTypes", userTypes);
+            return cacheContainer;
+        }
+    }
+}
