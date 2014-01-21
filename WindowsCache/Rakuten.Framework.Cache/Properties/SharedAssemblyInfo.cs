190167d70217e585401e82895d9c9f75a65b10a6
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-21 18:25:56 +0900

    Version management added All version sepcific data is linked to version files of Cache Core compoonent

---
+++ WindowsCache/Rakuten.Framework.Cache/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Windows Cache")]
+[assembly: AssemblyCopyright("Copyright © Rakuten Inc. 2014")]
+[assembly: AssemblyCompany("Rakuten Inc.")]
+[assembly: AssemblyTrademark("")]
