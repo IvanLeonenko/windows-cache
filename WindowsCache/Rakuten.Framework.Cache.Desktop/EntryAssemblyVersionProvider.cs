ab5260f85f16ca0b35d7816a2fc67fbeb591616b
Ivan Leonenko <ivan.leonenko@gmail.com>
2014-01-23 18:13:56 +0900

    Fix version provider for desktop component

--- WindowsCache/Rakuten.Framework.Cache.Desktop/EntryAssemblyVersionProvider.cs
+++ WindowsCache/Rakuten.Framework.Cache.Desktop/EntryAssemblyVersionProvider.cs
@@ -7,7 +7,7 @@ namespace Rakuten.Framework.Cache.Desktop
     {
         public System.Version GetVersion()
         {
-            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetCallingAssembly();
             var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
             return new System.Version(fvi.FileVersion);
         }
